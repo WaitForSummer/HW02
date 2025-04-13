7d67482d520f9228dab56cc935ebd7b19de8f552
WaitForSummer <monstr5665@gmail.com>
2025-04-13 20:58:12 +0300

    updated hello_world.cpp with clang-format

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 #include <string>
 
+<<<<<<< HEAD
 int main() {
 	std::string name;
 	std::cout << "Enter your name, please: "; // пользователь вводит свое имя
 	std::cin >> name;
 	std::cout << "Hello world from " << name << "!" << std::endl;
 	return 0;
+=======
+int
+main()
+{
+  std::string name;
+  std::cout << "Enter your name, please: "; // user enter its name
+  std::cin >> name;
+  std::cout << "Hello world from " << name << "!" << std::endl;
+  return 0;
+>>>>>>> 042d850 (updated hello_world.cpp with clang-format)
 }

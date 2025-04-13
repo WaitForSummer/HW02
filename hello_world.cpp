78cf26a9ceb1c3a94da4d43e712d6fef6513de9b
Wait For-Summer <58130219+WaitForSummer@users.noreply.github.com>
2025-04-13 21:01:27 +0300

    Update hello_world.cpp с комментариями

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,7 +3,7 @@
 
 int main() {
 	std::string name;
-	std::cout << "Enter your name, please: "; // user enter its name
+	std::cout << "Enter your name, please: "; // пользователь вводит свое имя
 	std::cin >> name;
 	std::cout << "Hello world from " << name << "!" << std::endl;
 	return 0;

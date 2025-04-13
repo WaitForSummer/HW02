b9baf3b8233147225363385b16d904b71ed0a4d5
WaitForSummer <monstr5665@gmail.com>
2025-04-13 20:45:27 +0300

    updated hello_world.cpp with comment

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,7 +3,7 @@
 
 int main() {
 	std::string name;
-	std::cout << "Enter your name, please: ";
+	std::cout << "Enter your name, please: "; // user enter its name
 	std::cin >> name;
 	std::cout << "Hello world from " << name << "!" << std::endl;
 	return 0;
